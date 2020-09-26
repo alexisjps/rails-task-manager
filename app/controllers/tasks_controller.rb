571a7d1cf9b3d92286e66c32666fada708161a03
alexis <alexis@hellomybusiness.fr>
2020-09-26 17:06:14 +0200

    fonctionne

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,34 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+def create
+  @task = Task.new(task_params)
+  @task.save
+  redirect_to task_path(@task)
+end
+
+  def new
+    @task = Task.new
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+def edit
+  @task= Task.find(params[:id])
+  @task.assign_attributes(task_params)
+  @task.save
+
+  redirect_to task_path
+end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  delete '/tasks/:id', to: 'tasks#destroy'
   get '/tasks', to: 'tasks#index'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
+  post '/tasks', to: 'tasks#create'
+  # path '/tasks/:id', to: 'task#edit'
 end
